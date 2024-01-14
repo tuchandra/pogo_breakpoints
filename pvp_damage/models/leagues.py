8f0ba7524eec3ccbe300761d1895499db7d72476
Tushar Chandra <me@tusharc.dev>
2024-01-13 20:17:58 -0600

    feat: remove ultra premier, RIP

--- pvp_damage/models/leagues.py
+++ pvp_damage/models/leagues.py
@@ -27,10 +27,6 @@ def _get_meta_from_file(name: str) -> list[tuple[PokemonSpecies, Moveset]]:
     return meta_list
 
 
-# LITTLE_CUP = League(name="Little Cup", max_cp=500, meta=[])
 GREAT_LEAGUE = League(name="Great League", max_cp=1500, meta=_get_meta_from_file("great"))
-GREAT_REMIX = League(name="Great League", max_cp=1500, meta=_get_meta_from_file("great_remix"))
 ULTRA_LEAGUE = League(name="Ultra League", max_cp=2500, meta=_get_meta_from_file("ultra"))
-ULTRA_PREMIER = League(name="Ultra League", max_cp=2500, meta=_get_meta_from_file("ultra_premier"))
-ULTRA_REMIX = League(name="Ultra League", max_cp=2500, meta=_get_meta_from_file("ultra_remix"))
 MASTER_LEAGUE = League(name="Master League", max_cp=10_000, meta=_get_meta_from_file("master"))
--- tests/test_leagues.py
+++ tests/test_leagues.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pvp_damage.models.leagues import GREAT_LEAGUE, ULTRA_LEAGUE, ULTRA_PREMIER
+from pvp_damage.models.leagues import GREAT_LEAGUE, ULTRA_LEAGUE
 from pvp_damage.models.pokemon import get_species
 
 
@@ -68,40 +68,3 @@ def test_ultra_league(name: str, shadow: bool, expected_present: bool):
     species = get_species(name, shadow)
 
     assert (species in ultra_species) is expected_present
-
-
-@pytest.mark.parametrize(
-    ("name", "shadow", "expected_present"),
-    [
-        # no GL pokemon
-        ("Azumarill", False, False),
-        ("Bastiodon", False, False),
-        ("Wigglytuff", False, False),
-        # no legendaries
-        ("Registeel", False, False),
-        ("Melmetal", False, False),
-        ("Giratina (Altered)", False, False),
-        ("Articuno", False, False),
-        # regular pokemon
-        ("Talonflame", False, True),
-        ("Venusaur", False, True),
-        ("Escavalier", False, True),
-        ("Roserade", False, True),
-        ("Nidoqueen", False, True),
-        ("Jellicent", False, True),
-        ("Stunfisk (Galarian)", False, True),
-        # these shadows aren't there
-        ("Venusaur", True, False),
-        ("Cradily", True, False),
-        ("Ninetales", True, False),
-        # these shadows are
-        ("Swampert", True, True),
-        ("Machamp", True, True),
-        ("Abomasnow", True, True),
-    ],
-)
-def test_ultra_premier(name: str, shadow: bool, expected_present: bool):
-    premier_species = [item[0] for item in ULTRA_PREMIER.meta]
-    species = get_species(name, shadow)
-
-    assert (species in premier_species) is expected_present
